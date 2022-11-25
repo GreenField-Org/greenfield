af8d2a40f0aac2af20836c02890d46d7279201d4
greenervigil <daniel.julian.vigil@gmail.com>
2022-11-24 19:10:22 -0500

    updates to header

--- header.php
+++ header.php
@@ -82,9 +82,6 @@
 				<a href="https://github.com/jeffreyvr/greenfield"
 					class="w-full sm:w-auto flex-none bg-gray-900 text-white text-lg leading-6 font-semibold py-3 px-6 border border-transparent rounded-xl focus:ring-2 focus:ring-offset-2 focus:ring-offset-white focus:ring-gray-900 focus:outline-none transition-colors duration-200">View
 					on Github</a>
-				<a href="https://github.com/jeffreyvr/greenfield"
-				class="w-full sm:w-auto flex-none bg-gray-900 text-white text-lg leading-6 font-semibold py-3 px-6 border border-transparent rounded-xl focus:ring-2 focus:ring-offset-2 focus:ring-offset-white focus:ring-gray-900 focus:outline-none transition-colors duration-200">View
-				on Github</a>
 			</div>
 			<!-- End introduction -->
 		<?php } ?>
